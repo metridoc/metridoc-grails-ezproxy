a6ad2b0af8cf5e20b673c4e698ca2be5400f9229
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-10-10 16:48:49 +0000

    fixed the build config to use our plugin repository

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,8 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.repos.metridocRepo.url = "https://metridoc.googlecode.com/svn/plugins"
+grails.project.repos.default = "metridocRepo"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
