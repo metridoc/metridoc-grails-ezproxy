695f53a63de11d88f138a72918abdca5046af5b5
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-10-25 19:23:45 +0000

    finished up the epzproxy log domain object that will be used for ingestion

--- grails-app/domain/metridoc/ezproxy/EzproxyLog.groovy
+++ grails-app/domain/metridoc/ezproxy/EzproxyLog.groovy
@@ -12,12 +12,19 @@ class EzproxyLog {
     String state
     String country
     String city
+    String hashUrl
+    String hashRefUrl
     String url
     String refUrl
+    String httpMethod
+    Integer httpStatus
+    String ezproxyId
+
     Boolean archive
     Boolean doi
     Boolean pmid
-    Boolean sfx
+    Boolean login
+    Boolean forceArchive
 
     static mapping = {
         def grailsApplication = Holders.grailsApplication
@@ -27,22 +34,33 @@ class EzproxyLog {
                 datasource('ezproxy')
             }
         }
+
+        fileName(index: 'idx_ez_log_file_name')
+        patronId(index: 'idx_ez_log_patron_id')
+        country(index: 'idx_ez_log_country')
+        state(index: 'idx_ez_log_state')
+        city(index: 'idx_ez_log_city')
+        hashRefUrl(index: 'idx_ez_log_hashrefurl')
+        hashUrl(index: 'idx_ez_log_hashurl')
+        doi(index: 'idx_ez_doi')
+        pmid(index: 'idx_ez_pmid')
+        login(index: 'idx_ez_login')
     }
 
     static constraints = {
-        fileName: index: 'idx_ez_log_file_name'
         //size 64 accommodates for sha256 hashing
         ipAddress(maxSize: 64)
         //size 64 accommodates for sha256 hashing
         patronId(maxSize: 64, nullable: true)
-        patronId: index: 'idx_ez_log_patron_id'
         country(maxSize: 50, nullable: true)
-        country: index: 'idx_ez_log_country'
+        country(maxSize: 50, nullable: true)
         state(maxSize: 50, nullable: true)
-        state: index: 'idx_ez_log_state'
         city(maxSize: 50, nullable: true)
-        city: index: 'idx_ez_log_city'
         url(size: 1..Integer.MAX_VALUE)
         refUrl(size: 1..Integer.MAX_VALUE)
+        hashRefUrl(maxSize: 64)
+        hashUrl(maxSize: 64)
+        httpMethod(maxSize: 10)
+        ezproxyId(maxSize: 50)
     }
 }
