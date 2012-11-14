f5d39e898754ba56212c15fc7ec00bd5bed68370
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-11-14 15:29:59 +0000

    reverting to the old way of resolving metridoc-core via downloading from a repository.  linking to a file location is causing all kinds of problems related to transitive dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,3 @@
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -6,8 +5,6 @@ grails.project.repos.metridocRepo.url = "https://metridoc.googlecode.com/svn/plu
 grails.project.repos.default = "metridocRepo"
 
 grails.project.dependency.resolution = {
-
-    def resolveMetridocCoreRemotely = true
     // inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
@@ -26,32 +23,16 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
         grailsRepo "https://metridoc.googlecode.com/svn/plugins/"
     }
-
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.18'
     }
 
-    def fileSeparator = System.getProperty("file.separator")
-    def metridocCoreDirectory = new File("..${fileSeparator}metridoc-core")
-
-    def metridocCoreDirectoryExists = metridocCoreDirectory.exists() && metridocCoreDirectory.isDirectory()
-
-    if (metridocCoreDirectoryExists) {
-        grails.plugin.location.'metridoc-core' = "../metridoc-core"
-        resolveMetridocCoreRemotely = false
-        println "metridoc-core exists locally, will use the local version of metridoc-core instead of downloading it"
-    } else {
-        println "Could not find metridoc-core locally, will use the standard dependency mechanism to get it"
-    }
-
     plugins {
-        if (resolveMetridocCoreRemotely) {
-            compile (":metridoc-core:0.52-SNAPSHOT") {
-                exclude "xmlbeans"
-                changing = true
-            }
+        compile (":metridoc-core:0.52-SNAPSHOT") {
+            exclude "xmlbeans"
+            changing = true
         }
         build(":tomcat:$grailsVersion",
               ":release:2.0.3",
