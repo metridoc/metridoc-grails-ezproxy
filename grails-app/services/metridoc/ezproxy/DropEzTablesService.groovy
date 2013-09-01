26b5945e4e873cac44660d91a200a508b0cb26ac
Tommy Barker <thomas.p.barker@gmail.com>
2013-08-31 20:54:13 -0400

    add code stubs for the different jobs

---
+++ grails-app/services/metridoc/ezproxy/DropEzTablesService.groovy
@@ -0,0 +1,11 @@
+package metridoc.ezproxy
+
+import metridoc.core.MetridocJob
+
+class DropEzTablesService extends MetridocJob {
+
+    @Override
+    def configure() {
+        return null  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
